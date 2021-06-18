e2e0a0b85a59f2af1dc2fa6652205ae34533d5a0
Siskanti <72306689+Siskanti@users.noreply.github.com>
2021-06-18 23:16:32 +0800

    Add files via upload

---
+++ OperatorAritmatika.java
@@ -0,0 +1,36 @@
+package belajar.coding;
+
+public class OperatorAritmatika {
+    public static void main(String[] args){
+        System.out.println("Penjumlahan");
+        int sum = 2 + 3;
+        System.out.println("1.  2 + 3 = " + sum);
+        System.out.println();
+
+        System.out.println("Pengurangan");
+        int subtraction = 2 - 3;
+        System.out.println("2.  2 - 3 = " + subtraction);
+        System.out.println();
+
+        System.out.println("Perkalian");
+        int multi = 2 * 3;
+        System.out.println("3. 2 * 3 = " + multi);
+        System.out.println();
+
+        System.out.println("Pembagian");
+        int division = 30 / 2;
+        System.out.println("4. 30 / 2 = " + division);
+        System.out.println();
+
+        System.out.println("Habis bagi");
+        int quotient = 4 % 2;
+        System.out.println("5. 4 % 2 = " + quotient);
+        int quotients = 3 % 2;
+        System.out.println("6. 3 % 2 = " + quotients);
+        System.out.println();
+
+
+
+
+    }
+}
---
+++ OperatorAssignment.java
@@ -0,0 +1,14 @@
+package belajar.coding;
+
+public class OperatorAssignment {
+    public static void main(String[] args){
+
+        int value = 4;
+
+        int anothervalue = 20;
+
+        System.out.println("The value is " + value);
+
+        System.out.println("Another value is " + anothervalue);
+    }
+}
